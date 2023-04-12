e60874410b7831956aab2cd1f24f2c35103856ae
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-04-12 20:14:18 +0300

    Add response errors, validation errors and user roles enum

--- helpers/constants/index.js
+++ helpers/constants/index.js
@@ -1,6 +1,11 @@
-const { CATEGORY_LIST, NOTICE_CATEGORIES } = require("./constans");
+const { CATEGORY_LIST, NOTICE_CATEGORIES } = require("./vacancies-categoryes");
+const { ROLES_LIST, USER_ROLES } = require("./user-roles");
+const { RESPONSE_ERRORS } = require("./response-errors");
 
 module.exports = {
   CATEGORY_LIST,
   NOTICE_CATEGORIES,
+  ROLES_LIST,
+  USER_ROLES,
+  RESPONSE_ERRORS,
 };
