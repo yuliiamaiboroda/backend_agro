e6165a4a82e3810cf501262dc6ffb1629c474d2c
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-04-14 09:14:25 +0300

    Fixed authorization for create route

--- routes/api/services.js
+++ routes/api/services.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { controllerExceptionWrapper } = require("../../helpers/utils");
+const { authUser } = require("../../middlewares");
 
 const { getAll, create } = require("../../controllers/services");
 
 router
   .get("/getAll", controllerExceptionWrapper(getAll))
-  .post("/create", controllerExceptionWrapper(create));
+  .post("/create", authUser, controllerExceptionWrapper(create));
 
 module.exports = router;
-
