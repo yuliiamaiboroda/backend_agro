d9451c296dca7dbd66e0f4249f7b97cdb3a3b82a
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-04-19 17:38:47 +0300

    Pulled from github

---
+++ middlewares/upload-product-image.middleware 2.js
@@ -0,0 +1,24 @@
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+const { CLOUDINARY_NAME, CLOUDINARY_KEY, CLOUDINARY_SECRET } = process.env;
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_NAME,
+  api_key: CLOUDINARY_KEY,
+  api_secret: CLOUDINARY_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: "avatars",
+  allowedFormats: ["jpg", "png"],
+  filename: (req, file, cb) => {
+    cb(null, uuidv4());
+  },
+});
+
+const productUploader = multer({ storage });
+
+module.exports = { productUploader };
