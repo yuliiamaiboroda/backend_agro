d167a1e22829d871893ba7806e3c87e2118a0d55
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-04-20 11:14:48 +0300

    separate cloudinary config

--- helpers/constants/index.js
+++ helpers/constants/index.js
@@ -1,9 +1,11 @@
 const { CATEGORY_LIST, NOTICE_CATEGORIES } = require("./vacancies-categoryes");
 const { ROLES_LIST, USER_ROLES } = require("./user-roles");
+const { cloudinary } = require("./cloudinary");
 
 module.exports = {
   CATEGORY_LIST,
   NOTICE_CATEGORIES,
   ROLES_LIST,
   USER_ROLES,
+  cloudinary,
 };
--- middlewares/upload-product-image.middleware.js
+++ middlewares/upload-product-image.middleware.js
@@ -1,20 +1,14 @@
-const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 const { v4: uuidv4 } = require("uuid");
-const { CLOUDINARY_NAME, CLOUDINARY_KEY, CLOUDINARY_SECRET } = process.env;
-
-cloudinary.config({
-  cloud_name: CLOUDINARY_NAME,
-  api_key: CLOUDINARY_KEY,
-  api_secret: CLOUDINARY_SECRET,
-});
+const { cloudinary } = require("../helpers/constants");
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: "products",
-    allowedFormats: ["jpg", "jpeg", "png"],
+    // allowedFormats: ["jpg", "jpeg", "png"],
+    format: async (req, file) => "jpg",
     public_id: async (req, file) => uuidv4(),
   },
 });
