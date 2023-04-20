7479e198ec91deeb6378cae8672fcbd491184918
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-04-20 10:58:26 +0300

    Add validataion

--- helpers/schemas/auth-schemas/login.schema.js
+++ helpers/schemas/auth-schemas/login.schema.js
@@ -1,4 +1,6 @@
 const Joi = require("joi");
+const { FieldErrors } = require("../../utils");
+
 const userLogInSchema = Joi.object({
   email: Joi.string()
     .trim()
@@ -6,14 +8,33 @@ const userLogInSchema = Joi.object({
     .min(10)
     .max(63)
     .email()
-    .required(),
+    .required()
+    .messages(
+      new FieldErrors("email")
+        .string()
+        .pattern("capital letter", "small letter and number")
+        .min(10)
+        .max(63)
+        .email()
+        .required()
+        .get()
+    ),
   password: Joi.string()
     .trim()
     .pattern(/^\d*(?=.*[a-z])(?=.*[A-Z])\S+\D*\d*$/)
     .min(7)
     .max(32)
-    .required(),
-});
+    .required()
+    .messages(
+      new FieldErrors("password")
+        .string()
+        .pattern("capital letter", "small letter and number")
+        .min(7)
+        .max(32)
+        .required()
+        .get()
+    ),
+}).messages(new FieldErrors("login").object().extraFields().get());
 
 module.exports = {
   userLogInSchema,
