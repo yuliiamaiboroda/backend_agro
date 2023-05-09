dbc112ac5cc73b8a428787802d436cc0d817865a
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2023-05-09 16:24:45 +0300

    delete extra request to db

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,15 +1,6 @@
-const { UserModel } = require("../../models");
-const { NotFoundError } = require("../../helpers/utils");
-
 const current = async (req, res, next) => {
-  const { id } = req.user;
-  const currentUser = await UserModel.findById(id);
+  const { email, password, name, surname, role } = req.user;
 
-  if (!currentUser) {
-    throw new NotFoundError();
-  }
-
-  const { email, password, name, surname, role } = currentUser;
   res.status(200).json({ email, password, name, surname, role });
 };
 
