10cc25888e6141ca691dc7f5146f3cf741ceb5bb
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2023-05-09 16:36:10 +0300

    fix returned data

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,7 +1,7 @@
 const current = async (req, res, next) => {
-  const { email, password, name, surname, role } = req.user;
+  const { email, name, surname, role } = req.user;
 
-  res.status(200).json({ email, password, name, surname, role });
+  res.status(200).json({ email, name, surname, role });
 };
 
 module.exports = {
