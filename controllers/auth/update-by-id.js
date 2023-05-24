fcd8075a7f381e292da528cdda3ba17c91cf85d3
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2023-05-24 12:57:26 +0300

    add in update by id ability to change password user(not required)

--- controllers/auth/update-by-id.js
+++ controllers/auth/update-by-id.js
@@ -1,5 +1,6 @@
 const { UserModel } = require("../../models");
 const { NotFoundError, EmailUsedError } = require("../../helpers/utils");
+const bcrypt = require("bcrypt");
 
 const updateById = async (req, res, next) => {
   const { id } = req.params;
@@ -9,12 +10,19 @@ const updateById = async (req, res, next) => {
     name: newName,
     surname: newSurname,
     role: newRole,
+    password,
   } = req.body;
 
   const oldUser = await UserModel.findById(id);
 
   if (!oldUser) {
     throw new NotFoundError();
+  }
+
+  let passwordHash;
+
+  if (password) {
+    passwordHash = await bcrypt.hash(password, 10);
   }
 
   try {
@@ -26,7 +34,13 @@ const updateById = async (req, res, next) => {
       id: userId,
     } = await UserModel.findByIdAndUpdate(
       id,
-      { email: newEmail, name: newName, surname: newSurname, role: newRole },
+      {
+        email: newEmail,
+        name: newName,
+        surname: newSurname,
+        role: newRole,
+        passwordHash: password ? passwordHash : oldUser.passwordHash,
+      },
       { returnDocument: "after", runValidators: true }
     );
 
