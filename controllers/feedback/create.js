9a194232655b2112fdddcdc5b43b1b60b937ac19
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2023-05-30 13:04:23 +0300

    fix returned result

--- controllers/feedback/create.js
+++ controllers/feedback/create.js
@@ -2,12 +2,18 @@ const { FeedbackModel } = require("../../models");
 
 const create = async (req, res, next) => {
   const { name, contactPhone, contactMail, comment, agreement } = req.body;
-  const result = await FeedbackModel.create({
+  const newFeedback = await FeedbackModel.create({
     name,
     contactPhone,
     contactMail,
     comment,
     agreement,
+  });
+
+  const result = await FeedbackModel.findById(newFeedback._id, {
+    agreement: 0,
+    viewedBy: 0,
+    createdAt: 0,
   });
 
   res.status(201).json(result);
