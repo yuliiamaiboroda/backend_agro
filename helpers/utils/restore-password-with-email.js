f063e7990f19655e0183e104f279a1c67a118f43
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2023-06-27 15:53:01 +0300

    rewrite on promises

--- helpers/utils/restore-password-with-email.js
+++ helpers/utils/restore-password-with-email.js
@@ -7,28 +7,52 @@ const {
   MAIL_SENDER_EMAIL,
 } = process.env;
 
+const transporter = nodemailer.createTransport({
+  host: MAIL_SENDER_HOST,
+  port: MAIL_SENDER_PORT,
+  secure: true,
+  auth: {
+    user: MAIL_SENDER_EMAIL,
+    pass: MAIL_SENDER_PASSWORD,
+  },
+});
+
 const sendEmail = async (email, subject, text) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: MAIL_SENDER_HOST,
-      port: MAIL_SENDER_PORT,
-      secure: true,
-      auth: {
-        user: MAIL_SENDER_EMAIL,
-        pass: MAIL_SENDER_PASSWORD,
-      },
+    await new Promise((resolve, reject) => {
+      transporter.verify(function (error, success) {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          console.log("Connected to server");
+          resolve(success);
+        }
+      });
     });
 
-    await transporter.sendMail({
+    const mailData = {
       from: MAIL_SENDER_EMAIL,
       to: email,
       subject: subject,
       text: text,
+    };
+
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          console.log(info);
+          resolve(info);
+        }
+      });
     });
 
     console.log("email sent sucessfully");
   } catch (error) {
-    console.log(error, "email not sent  ");
+    console.log(error, "email not sent ");
     throw new Error(`${error}`);
   }
 };
