d582a519960e7b2fdbe6c86af7d0c320b36d9d05
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-07-01 10:41:34 +0300

    Refactored refresh token, get vacancies by category and fix send mail attributes

--- controllers/vacancies/get-by-category.js
+++ controllers/vacancies/get-by-category.js
@@ -1,22 +1,21 @@
-const { NOTICE_CATEGORIES } = require("../../helpers/constants");
+const { NOTICE_CATEGORIES, CATEGORY_LIST } = require("../../helpers/constants");
 const { NotFoundError } = require("../../helpers/utils");
 const { VacancyModel } = require("../../models");
 
 const getByCategory = async (req, res, next) => {
   const { categoryName } = req.params;
 
-  if (categoryName === "all-vacancies") {
-    const allVacancies = await VacancyModel.find();
-    res.status(200).json(allVacancies);
-  }
-
   if (!NOTICE_CATEGORIES.includes(categoryName)) {
     throw new NotFoundError();
   }
 
-  const vacanciesList = await VacancyModel.find({ category: categoryName });
-
-  res.status(200).json(vacanciesList);
+  if (categoryName === CATEGORY_LIST.all) {
+    const allVacancies = await VacancyModel.find();
+    res.status(200).json(allVacancies);
+  } else {
+    const vacanciesList = await VacancyModel.find({ category: categoryName });
+    res.status(200).json(vacanciesList);
+  }
 };
 
 module.exports = {
