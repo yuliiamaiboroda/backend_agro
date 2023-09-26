ad540461222c91e59c110d2f936888688b52e79f
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2023-09-26 13:39:18 +0300

    separate auth and users routers

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,25 +1,39 @@
-const { updateById } = require("./update-by-id");
-const { current } = require("./current");
-const { getAll } = require("./get-all");
-const { login } = require("./login");
-const { logout } = require("./logout");
-const { refreshUser } = require("./refresh-user");
-const { register } = require("./register");
-const { removeById } = require("./remove-by-id");
-const { updatePassword } = require("./update-password");
-const { getCertainById } = require("./get-certain-by-id");
-const { restorePassword } = require("./restore-password");
+const usersServices = require('../../services/users');
+
+const getAllUsers = async (req, res) => {
+  const users = await usersServices.getAllUsers(req.user.id);
+
+  res.status(200).json(users);
+};
+
+const createUser = async (req, res) => {
+  const user = await usersServices.createUser(req.body);
+
+  res.status(201).json(user);
+};
+
+const getUserById = async (req, res) => {
+  const user = await usersServices.getUserById(req.params.id);
+
+  res.status(200).json(user);
+};
+
+const removeUserById = async (req, res) => {
+  await usersServices.removeUserById(req.params.id);
+
+  res.status(204).send();
+};
+
+const updateUserById = async (req, res) => {
+  const user = await usersServices.updateUserById(req.params.id, req.body);
+
+  res.status(200).json(user);
+};
 
 module.exports = {
-  register,
-  login,
-  logout,
-  getAll,
-  removeById,
-  updateById,
-  refreshUser,
-  current,
-  updatePassword,
-  getCertainById,
-  restorePassword,
+  createUser,
+  getAllUsers,
+  removeUserById,
+  updateUserById,
+  getUserById,
 };
