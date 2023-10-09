714e76edfb81480042635bf7eabcba34c342ca3e
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2023-10-09 20:20:53 +0300

    fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { globalErrorHandler } = require('./middlewares');
 const corsOptions = {
   //  To allow requests from client
   origin: [
-    'http://localhost:3001',
+    'http://localhost:3000',
     'http://127.0.0.1',
     'http://104.142.122.231',
     'https://yuliiamaiboroda.github.io',
