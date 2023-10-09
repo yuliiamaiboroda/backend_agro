ca82cad3d3fad93cd27bca56f39bc8b518a1016d
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-10-09 12:02:02 +0300

    Remove console.log from validate body middleware

--- middlewares/validate-body.middleware.js
+++ middlewares/validate-body.middleware.js
@@ -3,9 +3,7 @@ const { removeCloudinaryFileByURL } = require('../helpers/utils');
 
 const validateBody = schema => {
   const fn = (req, res, next) => {
-    console.log('req.body: \n', JSON.stringify(req.body));
     const { error } = schema.validate(req.body);
-    console.log('error: \n', error);
     if (error) {
       removeCloudinaryFileByURL(req.file?.path);
       next(new ValidationError(error.message));
